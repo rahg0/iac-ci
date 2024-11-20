cf923a339f658ddaafac69856045412e326ea6bc
Raghu <raghu.esh@gmail.com>
2024-11-20 18:22:07 +0530

    Update es.tf

--- aws/es.tf
+++ aws/es.tf
@@ -1,1 +1,13 @@
 # Will Add contents soon...
+resource "aws_elasticsearch_domain" "monitoring-framework" {
+  domain_name           = "tg-${var.environment}-es"
+  elasticsearch_version = "2.3"
+
+  cluster_config {
+    instance_type            = "t2.small.elasticsearch"
+    instance_count           = 1
+    dedicated_master_enabled = false
+    dedicated_master_type    = "m4.large.elasticsearch"
+    dedicated_master_count   = 1
+  }
+}
